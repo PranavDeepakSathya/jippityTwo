ecb0183c2c4d871ac92726949d798de4fa25d4f2
PranavDeepakSathya <sathya.pranav.deepak@gmail.com>
2026-04-06 08:04:48 +0530

    Enhance setup script with numba and repository clones
    Added installation of numba and additional repositories for flash attention and mdlARC.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 python3 -m venv .venv
 source .venv/bin/activate
-
 pip install --upgrade pip setuptools wheel
 pip install nvcc4jupyter numpy jupyter_client ipykernel ipywidgets
 pip install triton
@@ -9,8 +8,21 @@ pip install torch
 pip install matplotlib
 pip install pandas
 pip install nvidia-cutlass-dsl
+pip install numba
 
+# Flash attention (sm_120 only for 5090/CUDA 13)
+git clone https://github.com/Dao-AILab/flash-attention.git flash-attention
+cd flash-attention
+FLASH_ATTN_CUDA_ARCHS="120" MAX_JOBS=16 pip install . --no-build-isolation
+cd ..
 
+# mdlARC
+git clone https://github.com/mvakde/mdlARC.git mdlARC
+cd mdlARC
+pip install -r requirements.txt
+cd ..
+
+# Intra-kernel profiler
 git clone https://github.com/yao-jz/intra-kernel-profiler.git /root/intra-kernel-profiler
 
 git config --global user.email sathya.pranav.deepak@gmail.com
